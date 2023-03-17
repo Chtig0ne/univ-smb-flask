d038dbf2b109ff08d0d3bc5c673f0326b6655f3d
ben pey <chtigone1@gmail.com>
2023-03-17 07:40:04 +0000

    maj

--- iptables.py
+++ iptables.py
@@ -4,5 +4,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("TP3_Peytraud_Benoit.html")
+
+@app.route("/start")
 def start():
     return render_template("TP3_Peytraud_Benoit.html")
+
+@app.route("/rules_nat")
+def rules_nat():
+    return render_template("Regle_de_NAT.html")
+    
+@app.route("/rules_nat_add")
+def rules_nat_add():
+    return render_template("Ajout_de_NAT.html")
