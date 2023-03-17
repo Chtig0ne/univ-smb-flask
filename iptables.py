05fdec58169456bdab806bed761360dcd0231f4d
ben pey <chtigone1@gmail.com>
2023-03-17 08:48:43 +0000

    maj

--- iptables.py
+++ iptables.py
@@ -1,5 +1,6 @@
 # save this as app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request,Response
+import base64
 
 app = Flask(__name__)
 
@@ -14,7 +15,29 @@ def start():
 @app.route("/rules_nat")
 def rules_nat():
     return render_template("Regle_de_NAT.html")
-    
+
 @app.route("/rules_nat_add")
 def rules_nat_add():
     return render_template("Ajout_de_NAT.html")
+
+def check(aut_header):
+    username="bon"
+    password ="jour"
+    encoded_uname_pass=aut_header.split() [-1]
+    if encoded_uname_pass==base64.b64decode(username + ":" + password) :
+        return True
+
+@app.route('/auth')
+def auth():
+    aut_header=request.header.get('Auth')
+    if aut_header and check(aut_header):
+        return "page confidentielle"
+    else:
+        resp=Response()
+        resp.headers['WWW-Authenticate']='Basic'
+        return resp, 401
+
+
+@app.route('/mdp',methods=['GET','POST'])
+def mdp():
+    return render_template("mdp.html")
